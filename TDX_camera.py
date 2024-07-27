c588d09d660324807cb7b373d736d35588f4064e
efferyJ <33830690+jeff-ray448@users.noreply.github.com>
2024-07-27 23:04:17 +0800

    Update TDX_camera.py

--- TDX_camera.py
+++ TDX_camera.py
@@ -1,6 +1,6 @@
 from tdx_proxy import TDXProxy
 
-proxy = TDXProxy(app_id="b12406018-21578451-da0e-4d27", app_key="6201510b-79c8-49ad-8558-6fb9432620c0")
+proxy = TDXProxy(app_id="", app_key="")
 
 
 def search_city(str):
